58f48c4c8f9594c428aa20fd4d3a1e5afd6cc2fb
gsj864 <seungjun080604@gmail.com>
2026-01-28 13:50:40 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
-// Firebase configuration (global)
-window.firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyA6cJ17ONNniVeARZkdSaKbOnuPyTR1iUA",
   authDomain: "calendar-7ed47.firebaseapp.com",
   projectId: "calendar-7ed47",
@@ -8,3 +15,7 @@ window.firebaseConfig = {
   appId: "1:473050962088:web:8b1c44232c72718f47ca9d",
   measurementId: "G-SHW5R0BMTK"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
